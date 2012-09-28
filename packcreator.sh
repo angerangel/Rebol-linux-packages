df6b5075f95a707e90e3a372c181f79b48ff65b0
Massimiliano Vessi <angerangel@gmail.com>
2012-09-28 13:13:28 +0200

    Now it's comlpete
    Good

--- packcreator.sh
+++ packcreator.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-rm rebol*.deb
+#removing old versions
+rm rebol*.deb 
 rm rebol*.tgz
 rm rebol*.rpm 
-
+#creating new ones
 dpkg-deb -b rebol
 dpkg-deb -b rebol_amd64 
 sudo alien --to-rpm --to-tgz rebol.deb 
