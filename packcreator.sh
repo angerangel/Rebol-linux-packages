22af864a13404fb173c783486cc22d2b42415732
Massimiliano Vessi <angerangel@gmail.com>
2013-01-18 15:08:56 +0100

    Now it's more debian compliant

--- packcreator.sh
+++ packcreator.sh
@@ -8,3 +8,9 @@ dpkg-deb -b rebol
 dpkg-deb -b rebol_amd64 
 sudo alien --to-rpm --to-tgz rebol.deb 
 sudo alien --to-rpm rebol_amd64.deb
+
+echo "remember to launch:"
+echo ""
+echo "git push origin master"
+echo ""
+echo "after renaming files"
